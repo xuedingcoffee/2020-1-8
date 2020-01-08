14e682da2ac5f80a0c1a8e536df146eafae99b34
yxy <1056115843@qq.com>
2020-01-08 09:30:30 +0800

    bbbaaaaaaa

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# 2020-1-8
-2020-1-8
+# app1
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    //动态路由
+    path: '/about/:uname/id/:id', 
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/dongtai.vue'),
+   },
+    {
+      path: '/qiantao/',
+      component: () => import('../views/qiantao.vue'),
+      children:[
+        {
+          path: 'p1',//  /public/p1  * 不要加 /不然就不匹配
+          component: () => import('../views/p1.vue')
+        },
+        {
+          path: 'p2',
+          component: () => import('../views/p2.vue')
+        },
+        //如果进入/public下没有指定的children,默认添加一个组件，可以在path上写个''
+        // {
+        //   path:'',
+        //   component: () => import('../views/p3.vue')
+        // }
+      ]
+    },
+   
+  ]
+  const router = new VueRouter({
+  mode: 'history',
+  //base: process.env.BASE_URL,
+  routes
+})
+
+export default router
